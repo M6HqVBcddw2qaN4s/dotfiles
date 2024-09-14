9dddb71f79a7f83ce7c78ab15ad975d922f2039a
Whited Williams <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2024-09-14 09:59:15 +0000

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+	# Commands to run in interactive sessions can go here
+	export PATH="$PATH:$HOME/.local/bin:$HOME/.local/sbin:$HOME/.local/share/flatpak/exports/bin:$HOME/.local/share/JetBrains/Toolbox/scripts"
+	export TERM="xterm"
+end
