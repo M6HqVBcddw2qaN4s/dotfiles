c35806426d82833ade94a387fd79751bb8f51d76
Whited Williams <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2024-10-13 23:29:02 +0000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,13 @@ if status is-interactive
 	export PATH="$PATH:$HOME/.local/bin:$HOME/.local/sbin:$HOME/.local/share/flatpak/exports/bin:$HOME/.local/share/JetBrains/Toolbox/scripts"
 	export TERM="xterm"
 end
+
+# Remove below if you don't want a hostname alias
+function fish_prompt
+	set -l cyan (set_color cyan)
+	set -l blue (set_color blue)
+	set -l yellow (set_color yellow)
+	set -l reset (set_color normal)
+
+	echo -n $cyan(whoami)$reset"@"$blue"archlinux"$reset" "$yellow(prompt_pwd)$reset"> "
+end
