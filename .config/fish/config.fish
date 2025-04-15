13c6a8cef7c32a45b7542cec3c69aae29c5d7492
Whited Williams <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2025-04-15 09:47:35 +0000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
-	export PATH="$PATH:$HOME/.local/bin:$HOME/.local/sbin:$HOME/.local/share/flatpak/exports/bin:$HOME/.local/share/JetBrains/Toolbox/scripts"
+	export PATH="$PATH:$HOME/.local/bin:$HOME/.local/sbin:$HOME/.local/share/flatpak/exports/bin:$HOME/.local/share/JetBrains/Toolbox/scripts:$HOME/.deno/bin:$HOME/.cargo/bin"
 	export TERM="xterm"
 end
 
-# Remove below if you don't want a hostname alias
+# Remove below if you dont want a hostname alias
 function fish_prompt
 	set -l cyan (set_color cyan)
 	set -l blue (set_color blue)
